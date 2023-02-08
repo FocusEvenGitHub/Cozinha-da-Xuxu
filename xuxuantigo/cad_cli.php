6d641f65e9738a071833aa2ea1287e78cbd0a0c8
FocusEvenGitHub <91390179+FocusEvenGitHub@users.noreply.github.com>
2023-02-07 23:26:16 -0300

    Add files via upload

---
+++ xuxuantigo/cad_cli.php
@@ -0,0 +1,20 @@
+
+<?php		
+    $nome=$_POST['nome'];
+	$end=$_POST['end'];    
+    $fone=$_POST['fone'];
+    $sexo=$_POST['sexo'];
+    $dn=$_POST['dn'];
+    $cpf=$_POST['cpf'];	
+    $sql="insert into cliente(nome,end,fone,cpf,dn,sexo) values('$nome','$end','$fone','$cpf','$dn','$sexo')";
+       $con=mysqli_connect("127.0.0.1","root","","xuxu");	
+
+		if (mysqli_connect_errno())
+		{echo "Failed to connect to MySQL: ".mysqli_connect_error();}
+
+        mysqli_query($con,$sql);
+		echo "<h2>Cliente cadastrado com sucesso!</h2><p><br>";                
+        mysqli_close($con);
+ ?>
+         <h2><a href="home.php">VOLTAR</a></h2>
+
---
+++ xuxuantigo/delcli.php
@@ -0,0 +1,15 @@
+<?php
+	$id=$_POST['id']; 
+   	 $sql = "delete from cliente where id='$id'";	
+
+    $con=mysqli_connect("127.0.0.1","root","","xuxu");    
+      	     
+            if (mysqli_connect_errno())
+            {echo "Failed to connect to MySQL:" .mysqli_connect_error(); }  
+
+            mysqli_query($con, $sql);
+	        echo "<center><h2>Cliente Deletado com sucesso!</center></h2><p><br>";           
+
+    mysqli_close($con);
+?> 
+              <center><h2><a href="altdelcli.php">VOLTAR</a></h2></center>
